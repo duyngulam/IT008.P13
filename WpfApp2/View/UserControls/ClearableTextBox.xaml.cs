5912b05d22422a315ebbaf2ddeb9529c8a9bf5ee
Nat-1405 <nguyenanhthi066@gmail.com>
2024-11-04 19:07:13 +0700

    background

---
+++ WpfApp2/View/UserControls/ClearableTextBox.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp2.View.UserControls
+{
+    public partial class ClearableTextBox : UserControl
+    {
+        public ClearableTextBox()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            txtInput.Clear();
+            txtInput.Focus();
+        }
+    }
+}
