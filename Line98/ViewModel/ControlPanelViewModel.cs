2e4a4a2f06d8b8af14bb914aad097068842fdd7a
Nat-1405 <nguyenanhthi066@gmail.com>
2024-12-25 03:02:35 +0700

    page navigation

--- Line98/ViewModel/ControlPanelViewModel.cs
+++ Line98/ViewModel/ControlPanelViewModel.cs
@@ -10,10 +10,12 @@ namespace Line98.ViewModel
     {
         public MainViewModel MainViewModel { get; set; }
         public MenuItemViewModel MenuItemViewModel { get; set; }
+        public LoadGameViewModel LoadGameViewModel { get; set; }
         public ControlPanelViewModel()
         {
             MainViewModel = new MainViewModel();
             MenuItemViewModel = new MenuItemViewModel();
+            LoadGameViewModel = new LoadGameViewModel();
         }
     }
 }
--- Line98/ViewModel/MainViewModel.cs
+++ Line98/ViewModel/MainViewModel.cs
@@ -31,12 +31,14 @@ namespace Line98.ViewModel
         public ICommand ShowMenuStyleCommand { get; set; }
         public ICommand ShowMenuScoreNormalCommand { get; set; }
         public ICommand ShowMenuHelpCommand { get; set; }
+        public ICommand ShowLoadGameCommand { get; set; }
         public ICommand ExitCommand { get; set; }
         private void ShowStart(object obj) => CurrentView = new StartViewModel();
         private void ShowMenu(object obj) => CurrentView = new MenuViewModel();
         private void ShowMenuStyle(object obj) => CurrentView = new MenuStyleViewModel();
         private void ShowMenuScoreNormal(object obj) => CurrentView = new MenuScoreNormalViewModel();
         private void ShowMenuHelp(object obj) => CurrentView = new MenuHelpViewModel();
+        private void ShowLoadGame(object obj) => CurrentView = new LoadGameViewModel();
         private void ExitAciton(object obj)
         {
             
@@ -49,6 +51,7 @@ namespace Line98.ViewModel
             ShowMenuStyleCommand = new ViewModelCommand(ShowMenuStyle);
             ShowMenuScoreNormalCommand = new ViewModelCommand(ShowMenuScoreNormal);
             ShowMenuHelpCommand = new ViewModelCommand(ShowMenuHelp);
+            ShowLoadGameCommand = new ViewModelCommand(ShowLoadGame);
             ExitCommand = new ViewModelCommand(ExitAciton);
 
             CurrentView = new StartViewModel();
