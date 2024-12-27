9fe8f35776ea166c2649810cb284bbbf970d9269
Nat-1405 <nguyenanhthi066@gmail.com>
2024-12-27 15:18:42 +0700

    Complete IngameUC volume

--- Line98/MainWindow.xaml.cs
+++ Line98/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Line98.Control;
 using Line98.Model;
 using Line98.ViewModel;
+using System.ComponentModel;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -18,14 +19,37 @@ namespace Line98
 {
     public partial class MainWindow : Window
     {
+        private System.Media.SoundPlayer _player;
+        private ControlPanelViewModel _viewModel;
         public MainWindow()
         {
             InitializeComponent();
 
+            _player = new System.Media.SoundPlayer("Resources/Background Music/Song 1.wav");
+            _viewModel = new ControlPanelViewModel();
+            DataContext = _viewModel;
+
             //BACKGROUND MUSIC
             System.Media.SoundPlayer player = new System.Media.SoundPlayer("Resources/Background Music/Song 1.wav");
-            player.Load();
-            player.PlayLooping();
+            _player.Load();
+            _player.PlayLooping();
+
+            _viewModel.InGameViewModel.PropertyChanged += ViewModel_PropertyChanged;
+        }
+        private void ViewModel_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(InGameViewModel.IsVolumeChecked))
+            {
+                if (_viewModel.InGameViewModel.IsVolumeChecked)
+                {
+                    _player.Load();
+                    _player.PlayLooping();
+                }
+                else
+                {
+                    _player.Stop();
+                }
+            }
         }
     }
 }
